51dd3c6c0ca53f4bd0d4bd5e9fbc7c062494e5ea
juliangrewe-bosch <julian.grewe@de.bosch.com>
2023-08-31 16:07:41 +0200

    feat(test): add mp-spdz programs
    Signed-off-by: juliangrewe-bosch <julian.grewe@de.bosch.com>

--- src/test/scala/CarbynestackSimulation.scala
+++ src/test/scala/CarbynestackSimulation.scala
@@ -6,13 +6,13 @@
  */
 import io.carbynestack.amphora.client.Secret
 import io.carbynestack.amphora.common.{Tag, TagValueType}
-import io.gatling.app.Gatling
 import io.gatling.core.Predef._
-import io.gatling.core.config.GatlingPropertiesBuilder
 import org.gatling.plugin.carbynestack.PreDef._
 
+import java.math.BigInteger
 import scala.jdk.CollectionConverters._
 import scala.util.Random
+
 class CarbynestackSimulation extends Simulation {
 
   val apolloFqdn = sys.env.get("APOLLO_FQDN") match {
@@ -34,8 +34,8 @@ class CarbynestackSimulation extends Simulation {
     .invR("133854242216446749056083838363708373830")
     .program("ephemeral-generic.default")
 
-  val jeffTag: java.util.List[Tag] =
-    List(("billionaire", "jeff"))
+  val vectorATag: java.util.List[Tag] =
+    List(("vector", "a"))
       .map(
         x =>
           Tag
@@ -47,8 +47,8 @@ class CarbynestackSimulation extends Simulation {
       )
       .asJava
 
-  val elonTag: java.util.List[Tag] =
-    List(("billionaire", "elon"))
+  val vectorBTag: java.util.List[Tag] =
+    List(("vector", "b"))
       .map(
         x =>
           Tag
@@ -60,31 +60,41 @@ class CarbynestackSimulation extends Simulation {
       )
       .asJava
 
-  val jeffSecret: Array[java.math.BigInteger] = Array(new java.math.BigInteger("180"))
-  val elonSecret: Array[java.math.BigInteger] = Array(new java.math.BigInteger("177"))
+  val vectorAValues: Array[java.math.BigInteger] = Array.fill[java.math.BigInteger](10000)(new BigInteger("1"))
+  val vectorBValues: Array[java.math.BigInteger] = Array.fill[java.math.BigInteger](10000)(new BigInteger("2"))
 
-  val jeffsNetWorth = Secret.of(jeffTag, jeffSecret)
-  val elonsNetWorth = Secret.of(elonTag, elonSecret)
+  val vectorASecret = Secret.of(vectorATag, vectorAValues)
+  val vectorBSecret = Secret.of(vectorBTag, vectorBValues)
 
-  val code =
+  val multiplicationProgram =
     "port=regint(10000)\n" +
       "listen(port)\n" +
       "socket_id = regint()\n" +
       "acceptclientconnection(socket_id, port)\n" +
-      "v = sint.read_from_socket(socket_id, 2)\n" +
-      "first_billionaires_net_worth = v[0]\n" +
-      "second_billionaires_net_worth= v[1]\n" +
-      "result = first_billionaires_net_worth < second_billionaires_net_worth\n" +
+      "data = Array.create_from(sint.read_from_socket(socket_id, 20000))\n" +
+      "sum = Array(1, sint)\n" +
+      "@for_range(20000)\n" +
+      "def f(i):\n" +
+      "   data[i] = data[i]*2\n" +
+      "   sum[0] += data[i]\n" +
+      "sint.write_to_socket(socket_id, sum)"
+
+  val emptyProgram =
+    "port=regint(10000)\n" +
+      "listen(port)\n" +
+      "socket_id=regint()\n" +
+      "acceptclientconnection(socket_id, port)\n" +
+      "number = sint.read_from_socket(socket_id, 1)\n" +
       "resp = Array(1, sint)\n" +
-      "resp[0] = result\n" +
+      "resp[0] = number\n" +
       "sint.write_to_socket(socket_id, resp)"
 
-  val jeffFeeder = Array(
-    Map("secret" -> jeffsNetWorth)
+  val vectorAFeeder = Array(
+    Map("secret" -> vectorASecret)
   )
 
-  val elonFeeder = Array(
-    Map("secret" -> elonsNetWorth)
+  val vectorBFeeder = Array(
+    Map("secret" -> vectorBSecret)
   )
 
   val tags: java.util.List[Tag] =
@@ -112,28 +122,32 @@ class CarbynestackSimulation extends Simulation {
     Secret.of(tags, secrets)
   }
 
-  val feeder = Iterator.continually{
+  val feeder = Iterator.continually {
     Map("secret" -> generateSecretsFunction())
   }
 
-  val millionairesProblem = scenario("millionaires-problem-scenario")
-    .feed(jeffFeeder)
+  val emptyProgramScenario = scenario("empty-program-execution-scenario")
+    .feed(feeder)
     .exec(amphora.createSecret("#{secret}"))
-    .feed(elonFeeder)
-    .exec(amphora.createSecret("#{secret}"))
-    .exec(ephemeral.execute(code))
+    .exec(ephemeral.execute(emptyProgram))
 
-  val createSecretSoakTest = scenario("createSecret-soak-test-30min")
+  val multiplicationProgramScenario = scenario("multiplication-program-scenario")
+    .feed(vectorAFeeder)
+    .exec(amphora.createSecret("#{secret}"))
+    .feed(vectorBFeeder)
+    .exec(amphora.createSecret("#{secret}"))
+    .exec(ephemeral.execute(multiplicationProgram))
+
+  val createSecretSoakTestScenario = scenario("createSecret-soak-test-30min")
     .feed(feeder)
     .exec(amphora.createSecret("#{secret}"))
 
-  val createSecret = scenario("createSecret-scenario")
+  val createSecretScenario = scenario("createSecret-scenario")
     .feed(feeder)
     .exec(amphora.createSecret("#{secret}"))
 
   setUp(
-    createSecret.inject(atOnceUsers(1)).protocols(csProtocol)
-      .andThen(millionairesProblem.inject(atOnceUsers(1)).protocols(csProtocol))
+    multiplicationProgramScenario.inject(atOnceUsers(1)).protocols(csProtocol)
     /*.andThen(createSecretSoakTest.inject(constantUsersPerSec(550).during(60 * 30)))
     .protocols(csProtocol)*/
   )
