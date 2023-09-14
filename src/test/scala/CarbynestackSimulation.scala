04ca2ac9c618492a25320fde06272126dce920a6
juliangrewe-bosch <julian.grewe@de.bosch.com>
2023-09-14 16:40:06 +0200

    fix(CarbynestackSimulation): update simulations
    Signed-off-by: juliangrewe-bosch <julian.grewe@de.bosch.com>

--- src/test/scala/CarbynestackSimulation.scala
+++ src/test/scala/CarbynestackSimulation.scala
@@ -60,8 +60,8 @@ class CarbynestackSimulation extends Simulation {
       )
       .asJava
 
-  val vectorAValues: Array[java.math.BigInteger] = Array.fill[java.math.BigInteger](10000)(new BigInteger("1"))
-  val vectorBValues: Array[java.math.BigInteger] = Array.fill[java.math.BigInteger](10000)(new BigInteger("2"))
+  val vectorAValues: Array[java.math.BigInteger] = Array.fill[java.math.BigInteger](3)(new BigInteger("999"))
+  val vectorBValues: Array[java.math.BigInteger] = Array.fill[java.math.BigInteger](3)(new BigInteger("888"))
 
   val vectorASecret = Secret.of(vectorATag, vectorAValues)
   val vectorBSecret = Secret.of(vectorBTag, vectorBValues)
@@ -71,13 +71,12 @@ class CarbynestackSimulation extends Simulation {
       "listen(port)\n" +
       "socket_id = regint()\n" +
       "acceptclientconnection(socket_id, port)\n" +
-      "data = Array.create_from(sint.read_from_socket(socket_id, 20000))\n" +
-      "sum = Array(1, sint)\n" +
-      "@for_range(20000)\n" +
+      "data = Array.create_from(sint.read_from_socket(socket_id, 6))\n" +
+      "scalar_product = Array(1, sint)\n" +
+      "@for_range(3)\n" +
       "def f(i):\n" +
-      "   data[i] = data[i]*2\n" +
-      "   sum[0] += data[i]\n" +
-      "sint.write_to_socket(socket_id, sum)"
+      "   scalar_product[0] += data[i] * data[3 +i]\n" +
+      "sint.write_to_socket(socket_id, scalar_product)"
 
   val emptyProgram =
     "port=regint(10000)\n" +
@@ -131,23 +130,22 @@ class CarbynestackSimulation extends Simulation {
     .exec(amphora.createSecret("#{secret}"))
     .exec(ephemeral.execute(emptyProgram))
 
-  val multiplicationProgramScenario = scenario("multiplication-program-scenario")
+  val multiplicationProgramScenario = scenario("multiplication-program-execution-scenario")
     .feed(vectorAFeeder)
     .exec(amphora.createSecret("#{secret}"))
     .feed(vectorBFeeder)
     .exec(amphora.createSecret("#{secret}"))
     .exec(ephemeral.execute(multiplicationProgram))
 
-  val createSecretSoakTestScenario = scenario("createSecret-soak-test-30min")
-    .feed(feeder)
-    .exec(amphora.createSecret("#{secret}"))
-
-  val createSecretScenario = scenario("createSecret-scenario")
-    .feed(feeder)
-    .exec(amphora.createSecret("#{secret}"))
-
   setUp(
-    multiplicationProgramScenario.inject(atOnceUsers(1)).protocols(csProtocol)
+    emptyProgramScenario
+      .inject(atOnceUsers(1))
+      .protocols(csProtocol)
+      .andThen(
+        multiplicationProgramScenario
+          .inject(atOnceUsers(1))
+          .protocols(csProtocol)
+      )
     /*.andThen(createSecretSoakTest.inject(constantUsersPerSec(550).during(60 * 30)))
     .protocols(csProtocol)*/
   )
