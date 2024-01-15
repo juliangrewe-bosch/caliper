4bd012d9ff07a9e014f39755a8f21b09cdda5e76
juliangrewe-bosch <julian.grewe@de.bosch.com>
2024-01-15 03:14:46 +0100

    python script

--- mkdocs/docs/reports/amphora/createSecret_scenario/gatling.md
+++
@@ -1,9 +0,0 @@
-# Gatling
-
-## Response Times
-
-![Graph](../../../images/plots/gatling/amphora_latency.png)
-
-## Stats
-
-![Graph](../../../images/plots/amphora/createSecret_scenario/test.png)
--- scripts/generate_report.py
+++ scripts/generate_report.py
@@ -1,174 +1,119 @@
-import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+import os
 from prometheus_api_client import PrometheusConnect, MetricRangeDataFrame
 
-APOLLO_PROMETHEUS_CLIENT = PrometheusConnect(
-    url=f"http://{os.environ['APOLLO_NODE_IP']}:{os.environ['PROMETHEUS_METRICS_PORT']}", disable_ssl=True)
+# Path to the MkDocs folder
+#home_dir = os.environ['HOME']
+# AMPHORA_CHART_PATH = os.path.join(home_dir, 'caliper', 'mkdocs', 'docs', 'images', 'amphorasimulation')
+# AMPHORA_CHART_PATH = os.path.join(home_dir, 'caliper', 'mkdocs', 'docs', 'images', 'ephemeralsimulation')
 
-STARBUCK_PROMETHEUS_CLIENT = PrometheusConnect(
-    url=f"http://{os.environ['STARBUCK_NODE_IP']}:{os.environ['PROMETHEUS_METRICS_PORT']}", disable_ssl=True)
+APOLLO_PROMETHEUS_CLIENT = PrometheusConnect(url="http://20.54.34.230:9090", disable_ssl=True)
+STARBUCK_PROMETHEUS_CLIENT = PrometheusConnect(url="http://20.54.34.230:9090", disable_ssl=True)
 
-PROMETHEUS_RESOURCE_CONSUMPTION_QUERIES = [
-    # container_memory_working_set_bytes
-    ('container_memory_working_set_bytes_amphora',
-     'container_memory_working_set_bytes{container="amphora", pod=~"cs-amphora.*", service="kubelet"}'),
-    ('container_memory_working_set_bytes_castor',
-     'container_memory_working_set_bytes{container="castor", pod=~"cs-castor.*", service="kubelet"}'),
-    ('container_memory_working_set_bytes_ephemeral',
-     'container_memory_working_set_bytes{container="ephemeral-ephemeral", pod=~"ephemeral.*", service="kubelet"}'),
-    # container_cpu_usage_seconds_total
-    ('container_cpu_usage_seconds_total_amphora',
-     'irate(container_cpu_usage_seconds_total{container="amphora", pod=~"cs-amphora.*", service="kubelet"}[5m])'),
-    ('container_cpu_usage_seconds_total_castor',
-     'irate(container_cpu_usage_seconds_total{container="castor", pod=~"cs-castor.*", service="kubelet"}[5m])'),
-    ('container_cpu_usage_seconds_total_ephemeral',
-     'irate(container_cpu_usage_seconds_total{container="ephemeral-ephemeral", pod=~"ephemeral.*", ' \
-     'service="kubelet"}[5m])'),
-    # container_fs_writes_bytes_total
-    ('container_fs_writes_bytes_total_amphora',
-     'container_fs_writes_bytes_total{container="amphora", pod=~"cs-amphora.*", service="kubelet"}'),
-    ('container_fs_writes_bytes_total_castor',
-     'container_fs_writes_bytes_total{container="castor", pod=~"cs-castor.*", service="kubelet"}'),
-    ('container_fs_writes_bytes_total_ephemeral',
-     'container_fs_writes_bytes_total{container="ephemeral-ephemeral", pod=~"ephemeral.*", ' \
-     'service="kubelet"}'),
-    # container_fs_reads_bytes_total
-    ('container_fs_reads_bytes_total_amphora',
-     'container_fs_reads_bytes_total{container="amphora", pod=~"cs-amphora.*", service="kubelet"}'),
-    ('container_fs_reads_bytes_total_castor',
-     'container_fs_reads_bytes_total{container="castor", pod=~"cs-castor.*", service="kubelet"}'),
-    ('container_fs_reads_bytes_total_ephemeral',
-     'container_fs_reads_bytes_total{container="ephemeral-ephemeral", pod=~"ephemeral.*", ' \
-     'service="kubelet"}'),
-    # container_network_receive_bytes_total
-    ('container_network_receive_bytes_total_amphora',
-     'container_network_receive_bytes_total{pod=~"cs-amphora.*", service="kubelet"}'),
-    ('container_network_receive_bytes_total_castor',
-     'container_network_receive_bytes_total{pod=~"cs-castor.*", service="kubelet"}'),
-    ('container_network_receive_bytes_total_ephemeral',
-     'container_network_receive_bytes_total{pod=~"ephemeral.*", service="kubelet"}'),
-    # container_network_transmit_bytes_total
-    ('container_network_transmit_bytes_total_amphora',
-     'container_network_transmit_bytes_total{pod=~"cs-amphora.*", service="kubelet"}'),
-    ('container_network_transmit_bytes_total_castor',
-     'container_network_transmit_bytes_total{pod=~"cs-castor.*", service="kubelet"}'),
-    ('container_network_transmit_bytes_total_ephemeral',
-     'container_network_transmit_bytes_total{pod=~"ephemeral.*", service="kubelet"}')]
-
-PROMETHEUS_LATENCY_METRICS = [
-    ('amphora_latency',
-     'gatling_carbynestacksimulation_amphora{scope="all", metric="percentiles99"}'),
-    ('ephemeral_latency',
-     'gatling_carbynestacksimulation_ephemeral{scope="all", metric="percentiles99"}')]
-
-IMG_DIR = 'docs/assets/img/'
-END_TIME = datetime.now()
+END_TIME = datetime.now(timezone.utc)  # Prometheus uses UTC
 START_TIME = END_TIME - timedelta(hours=6)
-HTML_REPORT_TIMESTAMP = datetime.now().strftime("%B %d, %Y")
 
-plots_html = ''
-for query_name, query in PROMETHEUS_LATENCY_METRICS:
-    apollo_metrics_data_dict = APOLLO_PROMETHEUS_CLIENT.custom_query_range(query=query, start_time=START_TIME,
+AMPHORA_SIMULATION_PROMQL = 'caliper{simulation="amphorasimulation", metric="percentiles99", scope="ok"}'
+EPHEMERAL_SIMULATION_PROMQL = 'caliper{simulation="ephemeralsimulation", metric="percentiles99", scope="ok"}'
+
+AMPHORA_SERVICES = [("amphora", "cs-amphora"), ("castor", "cs-castor")]
+EPHEMERAL_SERVICES = [("ephemeral-ephemeral", "ephemeral"), ("castor", "cs-castor")]
+
+# The metric name is part of the file name
+CADVISOR_METRICS = [
+    'container_memory_working_set_bytes',
+    'container_cpu_usage_seconds_total',
+    'container_fs_writes_bytes_total',
+    'container_fs_reads_bytes_total',
+    'container_network_receive_bytes_total',
+    'container_network_transmit_bytes_total']
+
+CADVISOR_METRICS_TEMPLATE = [
+    # container_memory_working_set_bytes
+    'container_memory_working_set_bytes{{container="{container}", pod=~"{pod}.*", service="kubelet"}}',
+    # container_cpu_usage_seconds_total
+    'irate(container_cpu_usage_seconds_total{{container="{container}", pod=~"{pod}.*", service="kubelet"}}[5m])',
+    # container_fs_writes_bytes_total
+    'container_fs_writes_bytes_total{{container="{container}", pod=~"{pod}.*", service="kubelet"}}',
+    # container_fs_reads_bytes_total
+    'container_fs_reads_bytes_total{{container="{container}", pod=~"{pod}.*", service="kubelet"}}',
+    # container_network_receive_bytes_total
+    'container_network_receive_bytes_total{{pod=~"{pod}.*", service="kubelet"}}',
+    # container_network_transmit_bytes_total
+    'container_network_transmit_bytes_total{{pod=~"{pod}.*", service="kubelet"}}']
+
+
+def generate_cadvisor_metrics(services):
+    all_metrics = []
+
+    for container, pod in services:
+        metric = [metric_query.format(container=container, pod=pod)
+                  for metric_query in CADVISOR_METRICS_TEMPLATE]
+        all_metrics.extend(metric)
+
+    return all_metrics
+
+
+AMPHORA_CADVISOR_METRICS = generate_cadvisor_metrics(AMPHORA_SERVICES)
+EPHEMERAL_CADVISOR_METRICS = generate_cadvisor_metrics(EPHEMERAL_SERVICES)
+
+apollo_response_times_dict = APOLLO_PROMETHEUS_CLIENT.custom_query_range(query=AMPHORA_SIMULATION_PROMQL,
+                                                                         start_time=START_TIME,
+                                                                         end_time=END_TIME, step='15s')
+# Time ranges per group
+response_times_df = MetricRangeDataFrame(apollo_response_times_dict)
+response_times_df["timestamp"] = response_times_df.index
+
+gatling_groups_start_time = response_times_df.groupby("group")['timestamp'].min()
+gatling_groups_end_time = response_times_df.groupby("group")['timestamp'].max()
+
+for group in response_times_df['group'].drop_duplicates():
+    for metric_name, promQL in zip(CADVISOR_METRICS, AMPHORA_CADVISOR_METRICS):
+        apollo_cAdvisor_dict = APOLLO_PROMETHEUS_CLIENT.custom_query_range(query=promQL,
+                                                                           start_time=START_TIME,
                                                                            end_time=END_TIME, step='15s')
 
-    apollo_metrics_data_df = MetricRangeDataFrame(apollo_metrics_data_dict).drop_duplicates()
+        apollo_cAdvisor_df = MetricRangeDataFrame(apollo_cAdvisor_dict)
 
-    # Plotting
-    plt.figure(figsize=(12, 7))
-    plt.plot(apollo_metrics_data_df.index, apollo_metrics_data_df['value'] / 1000, 'ro')
+        # Plotting the data
+        plt.figure(figsize=(11, 7))
+        plt.plot(apollo_cAdvisor_df.index, apollo_cAdvisor_df['value'], label='Apollo')
 
-    plt.gca().xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%H:%M'))
-    plt.gca().xaxis.set_major_locator(plt.MaxNLocator(12))
-    plt.gcf().autofmt_xdate()
+        # Highlighting the area from the top to bottom of the plot, not limited to the line
+        plt.fill_betweenx([min(apollo_cAdvisor_df['value']), max(apollo_cAdvisor_df['value'])],
+                          (gatling_groups_start_time[group] - timedelta(minutes=2)),
+                          gatling_groups_end_time[group], color='orange', alpha=0.1)
 
-    plt.title(query_name)
-    plt.grid(True)
-    plt.subplots_adjust(bottom=0.1, top=0.95)
-    plot_path = os.path.join(IMG_DIR, f'{query_name}.png')
-    img_src_path = f'assets/img/{query_name}.png'
-    plt.savefig(plot_path)
-    plt.close()
+        # Adding grid
+        plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 
-    plots_html += f'''
-    <div class="graph-container">
-        <img src="{img_src_path}" alt="{query_name}">
-    </div>
-    '''
+        # Hiding the top and right spines
+        ax = plt.gca()  # Get the current Axes instance
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
 
-for query_name, query in PROMETHEUS_RESOURCE_CONSUMPTION_QUERIES:
-    apollo_metrics_data_dict = APOLLO_PROMETHEUS_CLIENT.custom_query_range(query=query, start_time=START_TIME,
-                                                                           end_time=END_TIME, step='15s')
+        # Set ticks direction to inward
+        ax.tick_params(axis='both', which='both', direction='in')
 
-    starbuck_metrics_data_dict = STARBUCK_PROMETHEUS_CLIENT.custom_query_range(query=query, start_time=START_TIME,
-                                                                               end_time=END_TIME, step='15s')
-
-    # groupby id in case of pod restarts and multiple time series for one query
-    apollo_metrics_data_df_grouped = MetricRangeDataFrame(apollo_metrics_data_dict).groupby('id')
-    starbuck_metrics_data_df_grouped = MetricRangeDataFrame(starbuck_metrics_data_dict).groupby('id')
-
-    # Plotting
-    plt.figure(figsize=(12, 7))
-
-    for label, df_grouped in [('Apollo', apollo_metrics_data_df_grouped),
-                              ('Starbuck', starbuck_metrics_data_df_grouped)]:
-        for _, df in df_grouped:
-            plt.plot(df.index, df['value'], label=label)
-
-    # Format the x-axis
-    plt.gca().xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%H:%M'))
-    if 'cpu' not in query_name:
+        # Formatting the plot
+        plt.tight_layout()
+        plt.gca().xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%H:%M'))
+        plt.gca().xaxis.set_major_locator(plt.MaxNLocator(12))
         plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(lambda value, pos: (
             f'{value / 1e9:.1f} GB' if value >= 1e9 else
             f'{value / 1e6:.0f} MB' if value >= 1e6 else
             f'{value / 1e3:.0f} KB' if value >= 1e3 else
             f'{value:.0f}'
         )))
-    plt.gca().xaxis.set_major_locator(plt.MaxNLocator(12))
-    plt.gcf().autofmt_xdate()
+        plt.legend(loc='best')
+        # adjust padding
+        plt.subplots_adjust(left=0.1)
 
-    plt.title(query_name)
-    plt.grid(True)
-    plt.legend()
-    plt.subplots_adjust(bottom=0.1, top=0.95)
-
-    plot_path = os.path.join(IMG_DIR, f'{query_name}.png')
-    img_src_path = f'assets/img/{query_name}.png'
-    plt.savefig(plot_path)
-    plt.close()
-
-    plots_html += f'''
-    <div class="graph-container">
-        <img src="{img_src_path}" alt="{query_name}">
-    </div>
-    '''
-
-# HTML Report
-html_content = f'''
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Caliper load-tests result</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
-</head>
-<body>
-<div class="label-container">
-    <span class="timestamp">Generated on {HTML_REPORT_TIMESTAMP}</span>
-</div>
-
-<div class="separator"></div>
-
-<div class="graph-container-wrapper">
-
-{plots_html}
-
-</div>
-
-</body>
-</html>
-'''
-
-with open('docs/index.html', 'w') as file:
-    file.write(html_content)
+        # if not os.path.isdir(AMPHORA_CHART_PATH):
+        #     os.makedirs(AMPHORA_CHART_PATH)
+        plt.savefig(group + "_" + metric_name)
+        plt.close()
